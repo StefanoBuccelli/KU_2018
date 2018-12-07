bf686440c12e8c0116d71648c00a0c0d675228e0
CPLAdmin <cpladmin@kumc.edu>
2018-12-07 11:23:38 -0600

    taking a look at the phase lag

--- play_with_data.m
+++ play_with_data.m
@@ -2,8 +2,19 @@
 clear
 clc
 close all
-cd('/Volumes/Processed_Data/Rat/ComplexBoxHealthyPilotStudy/R17-11/R17-11_2017_02_24_1/R17-11_2017_02_24_1_RawData')
-
+cd('P:\Rat\ComplexBoxHealthyPilotStudy\R17-13\R17-13_2017_02_18_1\R17-13_2017_02_18_1_RawData')
+d = designfilt('bandpassiir', ...       % Response type
+    'StopbandFrequency1',65, ...    % Frequency constraints
+    'PassbandFrequency1',70, ...
+    'PassbandFrequency2',95, ...
+    'StopbandFrequency2',100, ...
+    'StopbandAttenuation1',40, ...   % Magnitude constraints
+    'PassbandRipple',1, ...
+    'StopbandAttenuation2',50, ...
+    'DesignMethod','ellip', ...      % Design method
+    'MatchExactly','passband', ...   % Design method options
+    'SampleRate',20000);               % Sample rate
+% fvtool(d)
 %% retrive data
 all_chs=dir('*Ch*');
 
@@ -27,7 +38,8 @@ end
 for curr_ch=1:length(all_chs)
     disp([num2str(1e2*curr_ch/length(all_chs)) ' %'])
     curr_raw_DC(curr_ch,:)=curr_raw(curr_ch,:)-mean(curr_raw(curr_ch,:));
-    curr_low(curr_ch,:)=bandpass(curr_raw(curr_ch,:),[70 95],20e3,'Steepness',0.85,'StopbandAttenuation',60);
+    curr_low(curr_ch,:)=filtfilt(d,curr_raw(curr_ch,:));
+    %     curr_low(curr_ch,:)=bandpass(curr_raw(curr_ch,:),[70 95],20e3,'Steepness',0.85,'StopbandAttenuation',60);
     curr_low_dec(curr_ch,:)=downsample(curr_low(curr_ch,:),20);
     curr_hilb=hilbert(curr_low_dec(curr_ch,:));
     curr_angle(curr_ch,:)=angle(curr_hilb);
@@ -50,7 +62,37 @@ for curr_ch=1:length(all_chs)
 end
 linkaxes(h_f,'x')
 
+%% pick 2 channels
+figure
+subplot(2,1,1)
+plot(time_sec_dec,curr_low_dec(1,:),'b')
+hold on
+plot(time_sec_dec,curr_low_dec(18,:),'r')
+%%
+subplot(2,1,2)
+phase_lag=curr_angle(1,:)-curr_angle(18,:);
+[x,y]=pol2cart(phase_lag,1);
+compass(x,y)
+hold on
+[x,y]=pol2cart(curr_angle(1,2),1);
 
-
+%%
+figure
+for curr_window=1:100:length(x)
+    start=curr_window;
+    stop=curr_window+100;
+    subplot(2,1,1)
+    hold off
+    plot(1:101,curr_low_dec(1,start:stop),'b')
+    hold on
+    plot(1:101,curr_low_dec(18,start:stop),'r')
+    xlim([0 101])
+    xlabel('Time [ms]')
+    title(['Abs Start Win Time: ' num2str((start_sample+curr_window*20)/fs) ' s'])
+    subplot(2,1,2)
+    compass(x(start:stop),y(start:stop))
+    title('Phase Lag')
+    pause(1)
+end
 
 
